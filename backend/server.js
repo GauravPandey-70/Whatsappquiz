4b32dec644ecedf6148bb50c5de888750df82482
GauravPandey_80 <138811399+GauravPandey-70@users.noreply.github.com>
2025-09-20 13:17:15 +0530

    server.js

--- backend/server.js
+++ backend/server.js
@@ -14,9 +14,7 @@ const app = express();
 app.use(express.json());
 
 // Fallback to local URI if .env is not loaded
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb+srv://gauravpandey54909_db:<password>@cluster0.hbbuolp.mongodb.net/whatsappquiz
-
-';
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb+srv://gauravpandey54909_db:<password>@cluster0.hbbuolp.mongodb.net/whatsappquiz';
 
 const connectDB = async () => {
   try {
